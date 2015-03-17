3febcb316cc2081ec33236b4df460268feb330ff
Carson Gee <x@carsongee.com>
2015-03-17 19:03:34 -0400

    Add configuration

--- lmod_proxy/cmd.py
+++ lmod_proxy/cmd.py
@@ -3,11 +3,20 @@
 import logging
 import os
 
-import lmod_proxy
+from lmod_proxy import web
+
+log = logging.getLogger('lmod_proxy')
 
 
 def run_server():
     logging.basicConfig(level=logging.DEBUG)
     port = int(os.environ.get('PROXYLMOD_PORT', 5000))
     host = os.environ.get('PROXYLMOD_HOST', 'localhost')
-    lmod_proxy.app.run(debug=True, host=host, port=port)
+    log.info(
+        'Starting with configuration:\n %s',
+        '\n'.join([
+            '{0}: {1}'.format(x, y)
+            for x, y in sorted(dict(web.app.config).items())
+        ])
+    )
+    web.app.run(debug=True, host=host, port=port)
